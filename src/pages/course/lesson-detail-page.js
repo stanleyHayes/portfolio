30d48499f2729565b1ca9441ad591d349f593f3b
Stanley Hayford <hayfordstanley@gmail.com>
2021-05-23 18:30:02 +0000

    add lesson detail page

--- src/pages/course/lesson-detail-page.js
+++ src/pages/course/lesson-detail-page.js
@@ -1,16 +1,28 @@
 import React, {useState} from "react";
 import Layout from "../../components/layout";
-import {Box, Container, List, ListItem, makeStyles, Typography} from "@material-ui/core";
-import {useParams} from "react-router-dom";
+import {
+    Avatar,
+    Box,
+    Container,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    makeStyles,
+    Typography
+} from "@material-ui/core";
+import {useHistory, useParams} from "react-router-dom";
 import {getCourseLessonBySlug} from "../../data/data";
 
 const LessonDetailPage = () => {
 
-    const useStyles = makeStyles(() => {
+    const useStyles = makeStyles(theme => {
         return {
             container: {
                 paddingTop: 84,
-                paddingBottom: 84
+                paddingBottom: 84,
+                display: "flex",
+                maxWidth: '100vw'
             },
             page: {
                 textTransform: "uppercase"
@@ -27,46 +39,119 @@ const LessonDetailPage = () => {
             },
             iconContainer: {
                 marginBottom: 32
+            },
+            lessonTableOfContent: {
+                position: 'sticky',
+                top: 100
+            },
+
+            lessonContent: {
+                flexGrow: 1,
+                flexBasis: '75%',
+                maxHeight: '100vh',
+                overflow: 'scroll',
+                paddingTop: 32,
+                paddingBottom: 32
+            },
+            lessonsList: {},
+            lessonsContainer: {
+                flexBasis: '25%',
+                maxHeight: '100vh',
+                overflow: 'scroll',
+                position: 'sticky',
+                top: 100
+            },
+            tableOfContentTitle: {
+                fontWeight: "bold",
+                color: theme.palette.text.primary
+            },
+            listItem: {
+                ':Mui-selected': {
+                    backgroundColor: 'red'
+                }
+            },
+            lessonContainer: {
+                flexGrow: 1,
+                display: "flex"
+            },
+            content: {
+                flexGrow: 1
             }
         }
     });
 
     const classes = useStyles();
+    const history = useHistory();
+
     const {cslug, lslug} = useParams();
     const [course, lessons, lesson] = getCourseLessonBySlug(cslug, lslug);
     const [selectedLesson, setSelectedLesson] = useState(lesson);
     const handleSelectedLesson = lesson => {
         setSelectedLesson(lesson);
+        history.push(`/blog/${cslug}/lessons/${lesson.slug}`);
     }
     return (
         <Layout>
             <Box className={classes.container}>
-                <Box>
-                    <List>
+                <Box className={classes.lessonsContainer}>
+                    <List className={classes.lessonsList}>
                         {lessons && lessons.map((lesson, index) => {
                             return (
-                                <ListItem key={index} onClick={() => handleSelectedLesson(lesson)}>
-
+                                <ListItem
+                                    className={classes.listItem}
+                                    selected={lesson.slug === lslug}
+                                    button={true}
+                                    divider={true}
+                                    key={index}
+                                    onClick={() => handleSelectedLesson(lesson)}>
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <Typography variant="h6" color="textSecondary">{lesson.number}</Typography>
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <Typography
+                                                color="textSecondary"
+                                                variant="h6">
+                                                {lesson.title}
+                                            </Typography>
+                                        }
+                                        secondary={
+                                            <Typography
+                                                color="textSecondary"
+                                                variant="body2">
+                                                {lesson.summary}
+                                            </Typography>}
+                                    />
                                 </ListItem>
                             )
                         })}
                     </List>
                 </Box>
-                <Container>
-                    <Typography variant="h4">{course.name}</Typography>
-
+                <Box className={classes.lessonContent}>
                     {selectedLesson ? (
-                        <Box>
-                            <Typography variant="h4" align="center">{selectedLesson.title}</Typography>
-                        </Box>
+                        <Container className={classes.lessonContainer}>
+                            <Box className={classes.content} container={true}>
+                                <Typography color="textSecondary" variant="h4">{course.name}</Typography>
+                                <Typography
+                                    color="textSecondary"
+                                    variant="h4"
+                                    align="center">
+                                    {selectedLesson.title}
+                                </Typography>
+                            </Box>
+                            <Box item={true} className={classes.lessonTableOfContent} xs={12} md={9}>
+                                <Typography variant="body2" className={classes.tableOfContentTitle}>
+                                    Table of Contents
+                                </Typography>
+                            </Box>
+                        </Container>
                     ) : (
                         <Box>
 
                         </Box>
                     )}
-                </Container>
-                <Box>
-
                 </Box>
             </Box>
         </Layout>
